f54b3ad7ca740ce936b420ba52cc0c7d7537665d
zs0.0 <zsmmm999@gmail.com>
2024-09-26 11:29:21 +0100

    9.26.1

--- pre_ui_version/toolkit_web/try/python_try/myproject/drawboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import save_drawing
-
-urlpatterns = [
-    path('save_drawing/', save_drawing, name='save_drawing'),
-]
--- pre_ui_version/toolkit_web/try/python_try/project_try/backend/drawboard/urls.py
+++
@@ -1,7 +0,0 @@
-# backend/drawboard/urls.py
-from django.urls import path
-from .views import generate_svg
-
-urlpatterns = [
-    path('generate_svg/', generate_svg, name='generate_svg'),
-]
